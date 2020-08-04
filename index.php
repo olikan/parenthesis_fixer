4a466094233aefa760e88f5c550375de476de1a3
olikan <olga@accbox.info>
2020-08-05 02:35:10 +0300

    add invalid exception

--- index.php
+++ index.php
@@ -4,6 +4,6 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 $string = "(()()()()))((((()()()))(()()()(((()))))))";
 $match = \src\Parenthesis::hasMatched($string);
-var_dump($match);
 
+echo $match;
 
--- src/Parenthesis.php
+++ src/Parenthesis.php
@@ -2,6 +2,12 @@
 
 namespace src;
 
+use InvalidArgumentException;
+
+/**
+ * Class Parenthesis
+ * @package src
+ */
 class Parenthesis
 {
     /**
@@ -22,7 +28,11 @@ class Parenthesis
                     if (array_pop($stack) !== 0)
                         return false;
                     break;
-                default: break;
+                case (!preg_match('/(\s|\r|\n|\t)/', $string[$i]) ? true : false) :
+                    throw new InvalidArgumentException('Execption only accepts parentehesis or special entities. Input was: ' . $string[$i]);
+                    break;
+                default:
+                    break;
             }
         }
 
